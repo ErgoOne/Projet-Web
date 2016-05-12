9a33d5cc1e5500caf1b198c92333e96043def008
Hunters3rd <hunters3rd@yahoo.fr>
2016-05-12 17:48:14 +0200

    Début du gabarit de la page.
    Ajout des div, ainsi que les id modifiés dans le css.

--- tribu.fr/profil.php
+++ tribu.fr/profil.php
@@ -1,5 +1,28 @@
 <!-- Sandre : 11/05/2015 -->
 <?php include('entete_profil.php'); ?>
+	<div id="profil-top">
+			<!-- Contient photo/DateInscription/Pseudo/Région/Nombre de parties et victoires-->
+			<div id="profil-top-left"> 
+
+			</div>
+
+			<!-- Contient la liste d'amis -->
+			<div id="profil-top-right">
+
+			</div>
 		</div>
+
+		<div id="profil-bottom">
+			<!-- Contient le dernier carnet de bord -->
+			<div id="profil-bottom-left">
+
+			</div>
+
+			<!-- Contient les trophées -->
+			<div id="profil-bottom-right">
+
+			</div>
+		</div>
+	</div>
 <?php include('footer.php'); ?>
 </html>
