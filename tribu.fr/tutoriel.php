8067ce28ef44df559f576652602b723b46e57884
Sandre Dubois <sandre.dubois@hotmail.fr>
2016-05-12 16:23:57 +0200

    Structure page tutoriel fonctionnelle

--- tribu.fr/tutoriel.php
+++ tribu.fr/tutoriel.php
@@ -1,5 +1,26 @@
 <!-- Sandre : 11/05/2015 -->
 <?php include('entete_tutoriel.php'); ?>
+			<div id="first-tutoriel-container">
+				<h1>Deplacement, gestion bateaux</h1>
+				<div id="first-video-tutoriel-container">
+				</div>
+				<div id="first-text-tutoriel-container">
+				</div>
+			</div>
+			<div id="second-tutoriel-container">
+				<h1>Argent, pirates</h1>
+				<div id="second-text-tutoriel-container">
+				</div>
+				<div id="second-video-tutoriel-container">
+				</div>
+			</div>
+			<div id="third-tutoriel-container">
+				<h1>Ports</h1>
+				<div id="third-video-tutoriel-container">
+				</div>
+				<div id="third-text-tutoriel-container">
+				</div>
+			</div>
 		</div>
   </body>
 <?php include('footer.php'); ?>
