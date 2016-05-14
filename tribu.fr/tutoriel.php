4430d64cc241c478216898bcfc579df7bd0cd8cc
Sandre Dubois <sandre.dubois@hotmail.fr>
2016-05-14 18:43:27 +0200

    Pasge tutoriel OK

--- tribu.fr/tutoriel.php
+++ tribu.fr/tutoriel.php
@@ -1,24 +1,35 @@
-<!-- Sandre : 11/05/2015 -->
 <?php include('entete_tutoriel.php'); ?>
-			<div id="first-tutoriel-container">
+			<div id="tutoriel-container">
 				<h1>Deplacement, gestion bateaux</h1>
-				<div id="first-video-tutoriel-container">
+				<div id="video-tutoriel-container">
+					<iframe width="430px" height="290px" src="https://www.youtube.com/embed/ytWz0qVvBZ0" frameborder="0" allowfullscreen></iframe>
 				</div>
-				<div id="first-text-tutoriel-container">
+				<div id="text-tutoriel-container">
+					<p>Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.</p>
+					<p>Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.</p>
+					<p>Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.</p>
 				</div>
 			</div>
-			<div id="second-tutoriel-container">
+			<div id="tutoriel-container">
 				<h1>Argent, pirates</h1>
-				<div id="second-text-tutoriel-container">
+				<div id="text-tutoriel-container">
+					<p>Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.</p>
+					<p>Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.</p>
+					<p>Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.</p>
 				</div>
-				<div id="second-video-tutoriel-container">
+				<div id="video-tutoriel-container">
+					<iframe width="430px" height="290px" src="https://www.youtube.com/embed/ytWz0qVvBZ0" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
-			<div id="third-tutoriel-container">
+			<div id="tutoriel-container">
 				<h1>Ports</h1>
-				<div id="third-video-tutoriel-container">
+				<div id="video-tutoriel-container">
+					<iframe width="430px" height="290px" src="https://www.youtube.com/embed/ytWz0qVvBZ0" frameborder="0" allowfullscreen></iframe>
 				</div>
-				<div id="third-text-tutoriel-container">
+				<div id="text-tutoriel-container">
+					<p>Has autem provincias, quas Orontes ambiens amnis imosque pedes Cassii montis illius celsi praetermeans funditur in Parthenium mare, Gnaeus Pompeius superato Tigrane regnis Armeniorum abstractas dicioni Romanae coniunxit.</p>
+					<p>Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.</p>
+					<p>Oportunum est, ut arbitror, explanare nunc causam, quae ad exitium praecipitem Aginatium inpulit iam inde a priscis maioribus nobilem, ut locuta est pertinacior fama. nec enim super hoc ulla documentorum rata est fides.</p>
 				</div>
 			</div>
 		</div>
