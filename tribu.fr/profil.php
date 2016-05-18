45f9bfccd5ab9f99ca7bfed423e01bd743c48e5d
Hunters3rd <hunters3rd@yahoo.fr>
2016-05-18 17:13:38 +0200

    Précision des différents div utilisés.
    Prochainement : discussion avec la base de données.

--- tribu.fr/profil.php
+++ tribu.fr/profil.php
@@ -2,11 +2,23 @@
 			<div id="profil-top">
 					<!-- Contient photo/DateInscription/Pseudo/Région/Nombre de parties et victoires-->
 					<div id="profil-top-left">
+						<div id="profil-top-left-img">
+							<img height="250px" width="250px" src="images/screenshot3.jpg" alt="" />
+						</div>
 
+						<div id="profil-top-left-text">
+							<p> Pseudo<br/>
+								Région<br/>
+								Nombre de victoires : X<br/>
+								Nombre de parties : Y<br/></p>
+						</div>
 					</div>
 
 					<!-- Contient la liste d'amis -->
 					<div id="profil-top-right">
+						<h2>Liste d'amis</h2>
+
+						<!-- Liste affichage dynamique d'amis-->
 
 					</div>
 			</div>
@@ -14,11 +26,13 @@
 			<div id="profil-bottom">
 				<!-- Contient le dernier carnet de bord -->
 				<div id="profil-bottom-left">
+					<!-- Liste affichage dynamique des 3 derniers carnets de bord -->
 
 				</div>
 
 				<!-- Contient les trophées -->
 				<div id="profil-bottom-right">
+					<!-- Liste affichage dynamique des trophées-->
 
 				</div>
 			</div>
