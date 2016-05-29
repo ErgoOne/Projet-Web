2f2072c7f62ab0de74297ca238ea2be60ce303d9
Sandre Dubois <sandre.dubois@hotmail.fr>
2016-05-29 18:33:39 +0200

    Update entete_choix_salle.php

--- tribu.fr/entete_choix_salle.php
+++ tribu.fr/entete_choix_salle.php
@@ -21,15 +21,15 @@
   </head>
   <body>
 		<div id="main-container">
-      <a href="choix_salle.php">
+      <a href="/tribu.fr/choix_salle.php">
 			     <img id="logo" src="images/Logo_white.png">
       </a>
 			<nav>
         <ul>
-          <li class="menu_accueil"><a href="accueil.php">Accueil</a></li><!--
-          --><li class="menu_accueil"><a href="choix_salle.php">Jouer</a></li><!--
-          --><li class="menu_accueil"><a href="tutoriel.php">Tutoriel</a></li><!--
-          --><li class="menu_accueil"><a href="classement.php">Classement</a></li><!--
-          --><li class="menu_accueil"><a href="profil.php">Mon Compte</a></li>
+          <li class="menu_accueil"><a href="/tribu.fr/accueil.php">Accueil</a></li><!--
+          --><li class="menu_accueil"><a href="/tribu.fr/choix_salle.php">Jouer</a></li><!--
+          --><li class="menu_accueil"><a href="/tribu.fr/tutoriel.php">Tutoriel</a></li><!--
+          --><li class="menu_accueil"><a href="/tribu.fr/classement.php">Classement</a></li><!--
+          --><li class="menu_accueil"><a href="/tribu.fr/profil.php">Mon Compte</a></li>
         </ul>
 			</nav>
